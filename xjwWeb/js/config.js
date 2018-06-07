214b48453300b452e20de5daa1cd710fac2ebaac
ndljava <ndljava@126.com>
2018-06-07 12:17:49 +0800

    xjwweb

---
+++ xjwWeb/js/config.js
@@ -0,0 +1,12 @@
+var IMG_URL = 'http://img.xinjiangnet.com.cn/';
+var UPLOAD_URL = 'http://upload.xinjiangnet.com.cn/';
+var APP_URL = 'http://app.xinjiangnet.com.cn/';
+var WWW_URL = 'http://www.xinjiangnet.com.cn/';
+var SPACE_URL = 'http://space.xinjiangnet.com.cn/';
+
+var COOKIE_PRE = 'ECAXE_';
+var COOKIE_DOMAIN = '.xinjiangnet.com.cn';
+var COOKIE_PATH = '/';
+
+var SINA_APPKEY = '3972269122';
+var QQ_SOURCEID = '1000033';
